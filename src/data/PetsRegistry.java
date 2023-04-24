cc667ff19d78f622d7711a3017c3bb2ab3c4b0ea
RIP1982 <perovsky@list.ru>
2023-04-24 21:00:39 +0400

    add classes animals registry

---
+++ src/data/PetsRegistry.java
@@ -0,0 +1,26 @@
+package data;
+
+import java.util.ArrayList;
+
+public class PetsRegistry {
+    private ArrayList<Pet> petsList;
+
+    public PetsRegistry(ArrayList<Pet> petsList) {
+        this.petsList = petsList;
+    }
+
+    public ArrayList<Pet> getPetsList() {
+        return petsList;
+    }
+
+    public void setPetsList(ArrayList<Pet> petsList) {
+        this.petsList = petsList;
+    }
+
+    @Override
+    public String toString() {
+        return "PetsRegistry{" +
+                "petsList=" + petsList +
+                '}';
+    }
+}
